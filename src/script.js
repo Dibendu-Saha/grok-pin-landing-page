faa556b226d4f2ad4f7703f5adb0e9c38712a951
Dibendu Saha <65119411+Dibendu-Saha@users.noreply.github.com>
2025-05-10 01:34:12 +0530

    add complete pay and confirm order flow
    create order -> checkout -> pay -> redirect -> get pay status

--- src/script.js
+++ src/script.js
@@ -1,5 +1,26 @@
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("DOMContentLoaded");
+  getOrderStatus();
+});
+
+/* order id - store and fetch
+initTxn()
+  1. check localStorage for order_id
+  2. if present, remove
+  3. add new order_id
+
+confirmOrder()
+  1. check localStorage for order_id
+  2. if present, call checkOrderStatus() in document.addEventListener("DOMContentLoaded"...
+  2. retrieve order_id from localStorage
+  3. send with param/body
+  4. remove order_id from localStorage (MANDATORY)
+*/
+
 async function initTxn() {
-  const createOrderUrl = "http://localhost:8888/api/create-order";
+  const createOrderUrl = "http://localhost:8888/api/create-order",
+    returnUrl = "http://127.0.0.1:5500/index.html#user-data";
+
   const requestPayload = {
     customer_details: {
       customer_id: "CUSTOMER_UNIQUE_ID_12345",
@@ -9,7 +30,7 @@ async function initTxn() {
     order_currency: "INR",
     order_meta: {
       // return_url: "https://grok.com/",
-      return_url: "http://127.0.0.1:5500/index.html#user-data",
+      return_url: returnUrl,
     },
   };
 
@@ -27,6 +48,14 @@ async function initTxn() {
     }
 
     const data = await response.json();
+    console.log(data);
+
+    if (localStorage.getItem("orderId")) {
+      localStorage.removeItem("orderId");
+    }
+
+    localStorage.setItem("orderId", data.order_id);
+
     const cashfree = Cashfree({ mode: "sandbox" }); // "production" for live
     cashfree.checkout({
       paymentSessionId: data.payment_session_id,
@@ -34,5 +63,23 @@ async function initTxn() {
     });
   } catch (error) {
     console.error("Error:", error);
+  }
+}
+
+async function getOrderStatus() {
+  if (!localStorage.getItem("orderId")) return;
+
+  const orderId = localStorage.getItem("orderId"),
+    orderStatusUrl = "http://localhost:8888/api/create-order?orderId=" + orderId;
+
+  try {
+    const response = await fetch(orderStatusUrl),
+      data = await response.json();
+
+    console.log({ order_id: data.order_id, status: data.order_status });
+  } catch {
+    console.error("ERR!");
+  } finally {
+    localStorage.removeItem("orderId");
   }
 }
